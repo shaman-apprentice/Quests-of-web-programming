c82370081a9e88eb84dd887552cc7cffeed33cfc
Torsten Knauf <Torsten.Knauf@gmx.de>
2017-10-29 21:24:02 +0100

    init project with minimal webpack-dev-server

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+
+ReactDOM.render(
+    <h1>Hello, world asdfasdfsd!</h1>,
+    document.getElementById('App')
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.join(__dirname, "dist"),
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        template: './dist/index.html'
+    })],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+};
