795b44f79c50727e046b56258e00fa1911c875a6
Torsten Knauf <Torsten.Knauf@gmx.de>
2018-03-30 20:01:55 +0200

    css :hover for table of contents

--- docs/src/components/Table_of_contents/Styled_svg.js
+++ docs/src/components/Table_of_contents/Styled_svg.js
@@ -1,8 +1,16 @@
 import { Styled_opening_mechanism } from './Styled_opening_mechanism'
 
 export const Styled_svg = Styled_opening_mechanism.extend`
+  width: 100%;
+  
+  svg #routes > g {
+    fill: transparent;
+    transition: all 1s ease-in-out;
+    cursor: pointer;
+  }
   
   svg #routes > g:hover {
-    opacity: 0.5;
+    opacity: 0.75;
+    transform: scale(1.1);
   }
 `;
---
+++ docs/src/components/Table_of_contents/readme.md
@@ -0,0 +1,7 @@
+// it's important this outer g elem doesn't get any transformation,
+// because they would be overwritten by :hover css in Styled_svg.js
+<g id={routes_config.name} >
+    <g>/* drawings */</g>
+    // so mouse over is triggered also, when having mouse between a hole in drawing
+    <bounding_elem/>
+</g>
