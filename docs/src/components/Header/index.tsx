2af7f727c97d5103382ac85a4213108ad708e3cc
Torsten Knauf <Torsten.Knauf@gmx.de>
2018-04-02 21:47:52 +0200

    added title/hint to header

--- docs/src/components/Header/index.tsx
+++ docs/src/components/Header/index.tsx
@@ -16,7 +16,14 @@ export class Header extends React.Component<{}, {is_table_of_contents_shown: boo
     render() {
         return (
             <div onClick={this.toggle_table_of_contents}>
-                <Styled_Heading>Quests of Web Programming</Styled_Heading>
+                <Styled_Heading
+                    title={ this.state.is_table_of_contents_shown ?
+                        undefined :
+                        'click to open table of contents'
+                    }
+                >
+                    Quests of Web Programming
+                </Styled_Heading>
                 <Table_of_contents is_open={this.state.is_table_of_contents_shown} />
             </div>
         );
