07823a44ad15acee9555d4340618877bb27923a9
Torsten Knauf <Torsten.Knauf@gmx.de>
2018-04-08 19:43:33 +0200

    removed unnecessary grid

--- docs/src/Routes/Styled_route_wrapper.js
+++ docs/src/Routes/Styled_route_wrapper.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
 
 export const Styled_route_wrapper = styled.div`
-  display: grid;
-  align-items: center;
-  justify-content: center;
+  margin: auto;
+  width: 80%;
   
   padding: 2em;
 `;
