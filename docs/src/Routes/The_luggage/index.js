288471eca487aa09b12bbdcd32b022f90d124523
Torsten Knauf <Torsten.Knauf@gmx.de>
2018-04-27 16:36:53 +0200

    bunch of nice resources

--- docs/src/Routes/The_luggage/index.js
+++ docs/src/Routes/The_luggage/index.js
@@ -1,6 +1,34 @@
 import * as React from 'react'
 
 export const The_luggage = () =>
-    <div>
-        hi
+    <div style={{maxWidth: '75%', margin: 'auto'}}>
+        What hasn't been told so far, is that our main character is lazy. Why rewriting something,
+        smart people have already written down... Hence just a bunch of nice resources as treasure
+        at the end ot this quest:
+
+        <ul>
+            <li><a href="https://facebook.github.io/jest/">Jest</a> - the testing framework every
+                web developer should use.
+            </li>
+            <li><em>Clean Code</em> by Robert C. Martin and <em>Refactoring</em> by Martin Fowler -
+                2 Books every software developer should have read.
+            </li>
+            <li><a href="http://eloquentjavascript.net/">ELOQUENT JAVASCRIPT 3RG EDITION</a> -
+                I have actual read the 2nd edition, which gave me a great introduction to JS.
+                I am sure the 3rd is even more awesome.
+            </li>
+            <li><a href="https://www.youtube.com/watch?v=6q5-cVeNjCE">Nice talk</a>
+                &nbsp;about (how I would call it) post-it notes driven user stories.
+            </li>
+            <li><a href="https://www.youtube.com/watch?v=VNqmHJtItCs">Nice talk</a>
+                &nbsp;about dependency hell.
+            </li>
+            <li>Learning Node.js: A Hands-On Guide to Building Web Applications in JavaScript
+                (Addison-Wesley Learning) - Fancy but still easy introduction into Node.js and
+                basis http-server.
+            </li>
+            <li>The Deadline: A Novel About Project Management by Tom DeMarco - A continues series
+                of funny short stories about IT project management.
+            </li>
+        </ul>
     </div>
