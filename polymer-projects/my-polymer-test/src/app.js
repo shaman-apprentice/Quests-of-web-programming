97c5653a4bbbd9083e0d88bb514a4b5d186224ea
shaman-apprentice <Torsten.Knauf@gmx.de>
2018-06-26 22:27:29 +0200

    first baby steps with polymer

---
+++ polymer-projects/my-polymer-test/src/app.js
@@ -0,0 +1,13 @@
+import {PolymerElement, html} from '@polymer/polymer';
+
+class MyElement extends PolymerElement {
+  static get properties() { return { mood: String }}
+  static get template() {
+    return html`
+      <style> .mood { color: green; } </style>
+      Web Components are <span class="mood">[[mood]]</span>!
+    `;
+  }
+}
+
+customElements.define('my-element', MyElement);
