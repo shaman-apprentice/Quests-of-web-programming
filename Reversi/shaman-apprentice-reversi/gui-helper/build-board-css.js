3bd2610e6a8a3aa167b58e733948b9527a635ae4
shaman-apprentice <Torsten.Knauf@gmx.de>
2018-09-15 23:01:12 +0200

    basic css board

--- Reversi/shaman-apprentice-reversi/gui-helper/build-board-css.js
+++ Reversi/shaman-apprentice-reversi/gui-helper/build-board-css.js
@@ -2,8 +2,42 @@ shamanApprenticeReversiNs.tmp.buildBoardCss = function() {
   const style = document.createElement("style")
 
   style.innerHTML = `
+    .board {
+      min-width: 100px;
+      max-width: 500px;
+      display: grid;
+      background-color: black;
+      grid-template-columns: repeat(8, 1fr);
+      grid-gap: 2px;
+    }
+    
     .field {
-      display: inline-block;
+      position: relative;
+      background-color: darkgreen;
+    }
+    
+    .square-hack {
+      height: 0;
+      padding-bottom: 100%;
+    }
+    
+    .stone {
+      position: absolute;
+      display: hidden;
+      height: 95%;
+      width: 95%;
+      margin: 5%;
+      border-radius: 50%;
+    }
+    
+    .black {
+      display: block;
+      background-color: black;
+    }
+    
+    .white {
+      display: block;
+      background-color: white;
     }
   `
 
