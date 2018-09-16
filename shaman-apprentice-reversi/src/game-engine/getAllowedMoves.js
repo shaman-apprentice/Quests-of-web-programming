b2288653cf79c4aa6474c754075359a2c48bf5ae
shaman-apprentice <Torsten.Knauf@gmx.de>
2018-09-16 20:38:10 +0200

    working version of allowed moves

--- shaman-apprentice-reversi/src/game-engine/getAllowedMoves.js
+++ shaman-apprentice-reversi/src/game-engine/getAllowedMoves.js
@@ -1,3 +1,136 @@
-export default function(board) {
-  return [1, 1, 2, 3]
+export default function(board, turn) {
+  const allowedMoves = []
+
+  const isOwnStone = turn === "black" ? (i) => i === 1 : (i) => i === -1
+  const isOpponentStone = turn === "black" ? (i) => i === -1 : (i) => i === 1
+  const isNeutralStone = (i) => i === 0
+
+  for (let y = 0; y <= 7; y++) {
+    for (let x = 0; x <= 7; x++) {
+      const curr = board[y][x]
+
+      if (!isNeutralStone(curr) )
+        continue
+
+      // check right
+      if (x < 6 && isOpponentStone(board[y][x+1])) {
+        for (let xRight = x + 2; xRight <= 7; xRight++) {
+          if (isOwnStone(board[y][xRight])) {
+            allowedMoves.push(`${y}-${x}`)
+            break
+          }
+          else if (isNeutralStone(board[y][xRight]))
+            break
+        }
+      }
+
+      // check left
+      if (x > 1 && isOpponentStone(board[y][x-1])) {
+        for (let xLeft = x - 2; xLeft >= 0; xLeft--) {
+          if (isOwnStone(board[y][xLeft])) {
+            allowedMoves.push(`${y}-${x}`)
+            break
+          }
+          else if (isNeutralStone(board[y][xLeft]))
+            break
+        }
+      }
+
+      // check downwards
+      if (y < 6 && isOpponentStone(board[y+1][x])) {
+        for (let yDown = y + 2; yDown <= 7; yDown++) {
+          if (isOwnStone(board[yDown][x])) {
+            allowedMoves.push(`${y}-${x}`)
+            break
+          }
+          else if (isNeutralStone(board[yDown][x] )) {
+            break
+          }
+        }
+      }
+
+      // check upwards
+      if (y > 1 && isOpponentStone(board[y-1][x])) {
+        for (let yUp = y - 2; yUp >= 0; yUp--) {
+          if (isOwnStone(board[yUp][x])) {
+            allowedMoves.push(`${y}-${x}`)
+            break
+          }
+          else if (isNeutralStone(board[yUp][x] )) {
+            break
+          }
+        }
+      }
+
+      // check diagonal down right
+      if (y < 6 && x < 6 && isOpponentStone(board[y+1][x+1])) {
+        let yDiagonal = y + 2
+        let xDiagonal = x + 2
+        while (yDiagonal <= 7 && xDiagonal <= 7) {
+          if (isOwnStone(board[yDiagonal][xDiagonal])) {
+            allowedMoves.push(`${y}-${x}`)
+            break
+          }
+          else if (isNeutralStone(board[yDiagonal][xDiagonal])) {
+            break
+          }
+          yDiagonal += 1
+          xDiagonal += 1
+        }
+      }
+
+      // check diagonal down left
+      if (y < 6 && x > 1 && isOpponentStone(board[y+1][x-1])) {
+        let yDiagonal = y + 2
+        let xDiagonal = x - 2
+        while (yDiagonal <= 7 && xDiagonal >= 0) {
+          if (isOwnStone(board[yDiagonal][xDiagonal])) {
+            allowedMoves.push(`${y}-${x}`)
+            break
+          }
+          else if (isNeutralStone(board[yDiagonal][xDiagonal])) {
+            break
+          }
+          yDiagonal += 1
+          xDiagonal -= 1
+        }
+      }
+
+      // check diagonal up left
+      if (y > 1 && x > 1 && isOpponentStone(board[y-1][x-1])) {
+        let yDiagonal = y - 2
+        let xDiagonal = x - 2
+        while (yDiagonal >= 0 && xDiagonal >= 0) {
+          if (isOwnStone(board[yDiagonal][xDiagonal])) {
+            allowedMoves.push(`${y}-${x}`)
+            break
+          }
+          else if (isNeutralStone(board[yDiagonal][xDiagonal])) {
+            break
+          }
+          yDiagonal -= 1
+          xDiagonal -= 1
+        }
+      }
+
+      // check diagonal up right
+      if (y > 1 && x < 6 && isOpponentStone(board[y-1][x+1])) {
+        let yDiagonal = y - 2
+        let xDiagonal = x + 2
+        while (yDiagonal >= 0 && xDiagonal <= 7) {
+          if (isOwnStone(board[yDiagonal][xDiagonal])) {
+            allowedMoves.push(`${y}-${x}`)
+            break
+          }
+          else if (isNeutralStone(board[yDiagonal][xDiagonal])) {
+            break
+          }
+          yDiagonal -= 1
+          xDiagonal += 1
+        }
+      }
+
+    }
+  }
+  return allowedMoves
 }
--- shaman-apprentice-reversi/src/game-engine/getAllowedMoves.test.js
+++ shaman-apprentice-reversi/src/game-engine/getAllowedMoves.test.js
@@ -1,6 +1,6 @@
 import getAllowedMoves from "./getAllowedMoves"
 
-test("allowed moves in start position", () => {
+describe("allowed moves in start position", () => {
   const board = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -11,7 +11,73 @@ test("allowed moves in start position", () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]
+  const turn = "black"
 
-  const allowedMoves = getAllowedMoves(board)
-  expect(allowedMoves.length).toBe(4)
+  test("horizontal direction", () => {
+    const allowedMoves = getAllowedMoves(board, turn)
+    expect(allowedMoves).toContain("3-2")
+    expect(allowedMoves).toContain("4-5")
+  })
+
+  test("vertical direction", () => {
+    const allowedMoves = getAllowedMoves(board, turn)
+    expect(allowedMoves).toContain("2-3")
+    expect(allowedMoves).toContain("5-4")
+  })
+
+  test("4 moves are allowed", () => {
+    const allowedMoves = getAllowedMoves(board, turn)
+    expect(allowedMoves.length).toBe(4)
+  })
+})
+
+describe("diagonal allowed moves downwards", () => {
+  const board = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0,-1,-1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+  const turn = "black"
+
+  test("down right", () => {
+    const allowedMoves = getAllowedMoves(board, turn)
+    expect(allowedMoves).toContain("2-2")
+    expect(allowedMoves).toContain("2-3")
+  })
+
+  test("down left", () => {
+    const allowedMoves = getAllowedMoves(board, turn)
+    expect(allowedMoves).toContain("2-5")
+  })
+})
+
+describe("diagonal allowed moves upwards", () => {
+  const board = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0,-1,-1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+  const turn = "black"
+
+  test("up left", () => {
+    const allowedMoves = getAllowedMoves(board, turn)
+    expect(allowedMoves).toContain("5-5")
+    expect(allowedMoves).toContain("5-4")
+  })
+
+  test("up right", () => {
+    const allowedMoves = getAllowedMoves(board, turn)
+    expect(allowedMoves).toContain("5-2")
+    expect(allowedMoves).toContain("5-3")
+  })
 })
