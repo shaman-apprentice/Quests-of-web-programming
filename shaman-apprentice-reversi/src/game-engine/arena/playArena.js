282be3332c658aa295f5bb4e3742e83fd9946101
shaman-apprentice <Torsten.Knauf@gmx.de>
2018-10-01 23:05:02 +0200

    adjusted to better gameEngine scope

--- shaman-apprentice-reversi/src/game-engine/arena/playArena.js
+++ shaman-apprentice-reversi/src/game-engine/arena/playArena.js
@@ -1,13 +1,15 @@
 import playXGames from "./playXGames"
+import { createGameEngine } from "../model"
 
 /** @return {object} - { aiName1-aiName2: winquote of aiName1, who is black }*/
 export default (aiNames, times) => {
+  const gameEngine = createGameEngine()
   const aiCouunt = aiNames.length
   const winQuotes = {}
 
   for (let i = 0; i < aiCouunt; i++) {
     for (let j = i; j < aiCouunt; j++) {
-      winQuotes[`${aiNames[i]}-${aiNames[j]}`] = playXGames(aiNames[i], aiNames[j], times)
+      winQuotes[`${aiNames[i]}-${aiNames[j]}`] = playXGames(gameEngine, aiNames[i], aiNames[j], times)
     }
   }
 
